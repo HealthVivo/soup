c117e761531c0d020de78a09b9648a7c5eed772e
Colby Chiang <cc2qe@virginia.edu>
2014-09-13 19:08:40 -0400

    better mem performance

--- bamtofastq/bamtofastq.cpp
+++ bamtofastq/bamtofastq.cpp
@@ -41,15 +41,15 @@ char compBase (char base) {
   }
 }
 
-// Dictionary from http://stackoverflow.com/a/4384446
+// Hash table from http://stackoverflow.com/a/4384446
 struct nlist { /* table entry: */
   struct nlist *next; /* next entry in the chain */
   char *name; /* defined name */
   bam1_t *defn; /* replacement test */
 };
 
-#define HASHSIZE 1000
-  static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+#define HASHSIZE 15485863 /* best if a prime number */
+static struct nlist *hashtab[HASHSIZE]; /* pointer table */
 
 /* hash: form hash value for string s */
 unsigned hash(char *s) {
@@ -77,42 +77,50 @@ struct nlist *install(char *name, bam1_t *defn) {
     if (np == NULL || (np->name = strdup(name)) == NULL)
       return NULL;
     hashval = hash(name);
-    np->next = hashtab[hashval];
+    np->next = hashtab[hashval]; // unclear to me why this is necessary... seems redundant
     hashtab[hashval] = np;
   } else /* already there */
+    // free((void *) np->name); /* free previous name */
     free((void *) np->defn); /* free previous defn */
   if ((np->defn = bam_dup1(defn)) == NULL)
     return NULL;
   return np;
 }
 
+int *remove(char *name) {
+  struct nlist *np;
+  if ((np = lookup(name)) == NULL) { /* not found */
+    return NULL;
+  } else
+    free((void *) np->defn);
+}
+
 int print_fastq(bam1_t *bamrec, char read) {
-  int notPrimary = bamrec->core.flag & 256;
   int revStrand = bamrec->core.flag & 16;
   int i;
   uint8_t *readgroup = bam_aux_get(bamrec, "RG");
-  cout << "@" << bam1_qname(bamrec) << '/' << read << " RG:Z:" << readgroup+1 << endl; // +1 omits the first character (Z) indicating string
+  cout << "@" << bam1_qname(bamrec) << '/' << read << " RG:Z:" << readgroup+1 << '\n'; // +1 omits the first character (Z) indicating string
   if (!revStrand) { // forward strand
     for (i = 0; i < bamrec->core.l_qseq; ++i) {
       cout << bam_nt16_rev_table[bam1_seqi(bam1_seq(bamrec),i)] ;
     }
-    cout << "\n+" << endl;
+    cout << "\n+\n";
     
     for (i = 0; i < bamrec->core.l_qseq; ++i) {
       cout << (char) (bam1_qual(bamrec)[i] + 33) ;
     }
-    cout << endl;
+    cout << '\n';
   }
   else { // reverse strand
     for (i = bamrec->core.l_qseq - 1; i >= 0 ; --i) {
       cout << compBase(bam_nt16_rev_table[bam1_seqi(bam1_seq(bamrec),i)]) ;
     }
-    cout << "\n+" << endl;
+    cout << "\n+\n";
 	  
     for (i = bamrec->core.l_qseq - 1; i >= 0; --i) {
       cout << (char) (bam1_qual(bamrec)[i] + 33) ;
     }
-    cout << endl;
+    cout << '\n';
   }
   return 0;
 }
@@ -125,40 +133,46 @@ Program: bamtofastq\n\
 Version: 0.0.1\n\
 Authors: Ira Hall <ihall@genome.wustl.edu> and Colby Chiang <cc2qe@virginia.edu>\n\
 Description: Convert a coordinate sorted BAM file to FASTQ, while preserving\n\
-    read group information as a comment" << endl;
+    read group information as a comment" << '\n';
     cout << "\n\
-Usage: " << argv[0] << " [bamFile]\n" << endl;
+Usage: " << argv[0] << " <bamFile>\n" << '\n';
   }
   
   else {
     bam1_t *bamrec = bam_init1();
     samfile_t *samfile;
     if ( (samfile = samopen(argv[1], "rb", NULL)) == 0) {
-      cout << "error: could not open bam file" << endl;
+      cout << "error: could not open bam file\n";
       return 1;
     }
 
-    int flag, notPrimary, firstInPair, revStrand, i;
+    int notPrimary, firstInPair;
     while (samread(samfile, bamrec) > 0) {
+      notPrimary = bamrec->core.flag & 256;
+
+      // bam1_t *bamrec = malloc(sizeof(bam1_t));
       if (notPrimary) continue; // skip secondary alignments
 
       // add to hash table if mate already there
       nlist *mate = lookup(bam1_qname(bamrec));
       if (! mate) {
       	install(bam1_qname(bamrec), bamrec);
+	// free((void *) bamrec);
       	continue;
       }
-
       // print the FASTQ for matched pairs
-      int firstInPair = bamrec->core.flag & 64;
+      firstInPair = bamrec->core.flag & 64;
       if (firstInPair) {
-	print_fastq(bamrec, '1');
-	print_fastq(mate->defn, '2');
+      	print_fastq(bamrec, '1');
+      	print_fastq(mate->defn, '2');
       }
       else {
-	print_fastq(mate->defn, '1');
-	print_fastq(bamrec, '2');
+      	print_fastq(mate->defn, '1');
+      	print_fastq(bamrec, '2');
       }
+      
+      bam_destroy1(mate->defn);
+      
 
       // maybe this will free memory?
       // install(bam1_qname(bamrec), bamrec);
